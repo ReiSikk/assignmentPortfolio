6c6233503f9eb32c6d88361924c202ed3ea443cb
Rei Sikk <reix0010@stud.kea.dk>
2022-10-14 14:20:47 +0200

    setup

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import { animate, scroll, stagger, timeline } from "https://cdn.skypack.dev/motion";
+
+window.addEventListener("DOMContentLoaded", start);
+
+//* INITIALIZE
+async function start() {
+  document.querySelectorAll(".project_card").forEach((card) => card.addEventListener("mouseover", timeline));
+}
+
+// card animation
+const sequence = [
+  [".card_header", { translateY: [200, 0] }, { duration: 0.3 }],
+  [".card_header", { opacity: 1 }, { duration: 0.2 }, { at: -0.5 }],
+];
+scroll(timeline(sequence));
