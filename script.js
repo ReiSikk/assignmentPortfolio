b5e1c2d4905ad437ae830f67e4cd822533d7cf53
Rei Sikk <reix0010@stud.kea.dk>
2022-10-14 15:31:08 +0200

    homepage layout and content

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ window.addEventListener("DOMContentLoaded", start);
 
 //* INITIALIZE
 async function start() {
-  document.querySelectorAll(".project_card").forEach((card) => card.addEventListener("mouseover", timeline));
+  /*   document.querySelectorAll(".project_card").forEach((card) => card.addEventListener("mouseover", timeline)); */
+  console.log("start");
 }
 
 // card animation
 const sequence = [
   [".card_header", { translateY: [200, 0] }, { duration: 0.3 }],
-  [".card_header", { opacity: 1 }, { duration: 0.2 }, { at: -0.5 }],
+  [".card_header", { opacity: 1 }, { duration: 0.2 }, { at: "-0.5" }],
 ];
 scroll(timeline(sequence));
