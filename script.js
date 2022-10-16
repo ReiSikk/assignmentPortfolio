6d126a802e67016f6b16bd5aba34a2b4e04aeff0
Rei Sikk <reix0010@stud.kea.dk>
2022-10-16 21:09:13 +0200

    subpage content and styling fixes

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ window.addEventListener("DOMContentLoaded", start);
 //* INITIALIZE
 async function start() {
   /*   document.querySelectorAll(".project_card").forEach((card) => card.addEventListener("mouseover", timeline)); */
+
   console.log("start");
 }
 
@@ -16,3 +17,17 @@ const sequence = [
   [".card_header", { opacity: 1 }, { duration: 0.2 }, { at: "-0.5" }],
 ];
 scroll(timeline(sequence));
+
+const smallWindow = window.matchMedia("(max-width: 1100px)");
+function checkScreen() {
+  if (smallWindow.matches) {
+    document.querySelector(".mouse_container").style.display = "none";
+  } else {
+    document.querySelector(".mouse_container").style.display = "flex";
+  }
+}
+// Register event listener
+smallWindow.addListener(checkScreen);
+
+// Initial check
+checkScreen(smallWindow);
