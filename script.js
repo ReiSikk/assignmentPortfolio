ac378bab799def3fb644fe9b64c34925e0f607d8
Rei Sikk <reix0010@stud.kea.dk>
2022-10-16 23:01:15 +0200

    final touchups

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 "use strict";
 
-import { animate, scroll, stagger, timeline } from "https://cdn.skypack.dev/motion";
+import { animate, scroll, stagger, timeline, inView } from "https://cdn.skypack.dev/motion";
 
 window.addEventListener("DOMContentLoaded", start);
 
 //* INITIALIZE
 async function start() {
   /*   document.querySelectorAll(".project_card").forEach((card) => card.addEventListener("mouseover", timeline)); */
-
-  console.log("start");
+  inView(header, () => {
+    console.log("Element has entered the viewport");
+    timeline(sequence);
+  });
 }
 
 // card animation
+const header = document.querySelector(".card_header");
+
 const sequence = [
   [".card_header", { translateY: [200, 0] }, { duration: 0.3 }],
-  [".card_header", { opacity: 1 }, { duration: 0.2 }, { at: "-0.5" }],
+  [".card_header", { opacity: 1 }, { duration: 0.5 }, { at: "-0.5" }],
 ];
-scroll(timeline(sequence));
 
 const smallWindow = window.matchMedia("(max-width: 1100px)");
+
 function checkScreen() {
   if (smallWindow.matches) {
     document.querySelector(".mouse_container").style.display = "none";
@@ -28,6 +32,5 @@ function checkScreen() {
 }
 // Register event listener
 smallWindow.addListener(checkScreen);
-
 // Initial check
 checkScreen(smallWindow);
